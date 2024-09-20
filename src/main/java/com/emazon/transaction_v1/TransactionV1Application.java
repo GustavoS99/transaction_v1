f0fcb38ba29f3b4f87fd505f7ba1aeb8894edf18
Gustavo <desarrollador.gustavo@gmail.com>
2024-09-19 19:22:11 -0500

    init Transaction microservice

---
+++ src/main/java/com/emazon/transaction_v1/TransactionV1Application.java
@@ -0,0 +1,13 @@
+package com.emazon.transaction_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/emazon/transaction_v1/TransactionV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.transaction_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
